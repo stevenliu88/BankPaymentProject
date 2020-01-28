363993754a3c7e748a0d3931e298e85a276612ac
lei liu <liuleiwangyue@gmail.com>
2020-01-28 15:32:43 +1100

    Create models and established connection to mssql

---
+++ WebApi/WebApi/Models/APIDB.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+
+    public class APIDB: DbContext
+    {
+        public APIDB(DbContextOptions<APIDB> options) : base(options)
+        {
+
+        }
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+
+    }
+}
